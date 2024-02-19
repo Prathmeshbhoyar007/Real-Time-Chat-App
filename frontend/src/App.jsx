3c469302e58a015a8e61872def343b6e942f956d
Prathmesh <bhoyarpratham007@gmail.com>
2024-02-19 17:12:51 +0530

    Final Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/home";
-import Login from "./pages/login/login";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp"
 import { Toaster } from 'react-hot-toast'
 import { useAuthContext } from './context/AuthContex'
